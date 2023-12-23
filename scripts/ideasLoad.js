36e0525c406c8341c47a806601a87fbd5d739b31
kkk <edschipunova@gmail.com>
2023-12-23 23:12:27 +0300

    анимация загрузки + подгружаемые данные

---
+++ scripts/ideasLoad.js
@@ -0,0 +1,28 @@
+window.onload = async (event) => {
+    let index = timerStart % 40;
+    for (let i = 1; i <= 5; i++) {
+        try {
+            let response = await fetch("https://jsonplaceholder.typicode.com/todos/" + (i + index));
+            let json = await response.json();
+            let translator = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=la&tl=ru&dt=t&q="
+                + encodeURI(json["title"]));
+            let text = await translator.json();
+            let translatedText = await text[0][0][0]
+            await new Promise(resolve => setTimeout(resolve, 2000));
+            await (i === 5) ? addLastTask(translatedText) : addTask(translatedText);
+        } catch (error) {
+            alert(error);
+        }
+    }
+};
+
+function addLastTask(text) {
+    addTask(text);
+    let loader = document.getElementsByClassName("loader").item(0);
+    loader.parentElement.parentElement.classList.add("hidden");
+}
+
+function addTask(text) {
+    let ch = document.getElementById("ideas-display").children[1].lastElementChild;
+    ch.insertAdjacentHTML("beforebegin", "<tr><td>" + text + "</td></tr>");
+}
